04a58f7e8f8855791d4d904e9cbad10c631c6978
Salamander <sbelal@gmail.com>
2017-03-06 03:14:21 -0800

    Project 2

--- SentimentAnalysis/SentimentAnalysis.py
+++ SentimentAnalysis/SentimentAnalysis.py
@@ -1,1 +1,13 @@
-print("Hello world!")
+age = {"Sal":36, 'Naz':29}
+
+age["Naz"] = 30
+age["Baz"] = 32
+#del age["Sal"]
+
+
+print(age["Naz"])
+print(age)
+print(age["Sal"])
+print(age["Baz"])
+
+print(42 in age.values())
