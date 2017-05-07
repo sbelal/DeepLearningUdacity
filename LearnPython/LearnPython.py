694d3a8a8eeac9e7b698a184965ccbd39465f54d
Salamander <sbelal@gmail.com>
2017-05-06 23:04:26 -0700

    language translation

---
+++ LearnPython/LearnPython.py
@@ -0,0 +1,35 @@
+
+
+
+testInput = [[5, 14, 26, 26, 4], [22, 6, 17], [14, 19, 28, 27, 11]]
+
+print(testInput[0])
+
+
+
+
+
+
+
+
+
+
+
+#import tensorflow as tf;
+#print(tf.__version__)
+
+#x = tf.Variable([[[1, 1, 1], [2, 2, 2]],
+#             [[3, 3, 3], [4, 4, 4]],
+#             [[5, 5, 5], [6, 6, 6]]]
+#, tf.float32)
+
+
+#output = tf.strided_slice(x, [1, 0, 0], [2, 1, 3], [1, 1, 1])
+
+##sess = tf.Session()
+##print(sess.run([node1, node2]))
+
+#sess = tf.Session()
+#init = tf.global_variables_initializer()
+#y = sess.run(init)
+#print(sess.run(output))
